e55590b36668eebf8fbeb49af146729d1b3be966
Dalal Alshehri <Dalal Alshehri>
2016-02-15 11:43:00 -0600

    the last thing I could do

--- printing.java
+++ printing.java
@@ -56,31 +56,34 @@ static int processExpression(String y)
         {
     	   String current = "";//it helps to hold the current value
     	   int result;//the expression total will held in this variable
-    	   
+    	   int leftOp = 0; //this variable used to save the numbers on the left side of operator(=,-,*,/)
+    	   int rightOp = 0;//this variable used to save the numbers on the right side of operator(=,-,*,/)
 	       for(int i=0; i< y.length();i++)//for looping until the end of string 
               {
 	    	   current = current + y.charAt(i);
+	    	   
 	    	   if(y.indexOf(i)!= -1)//to check that the string still has value
 	    	     {
+	    		   //here I need to hold the numbers on specific place, but I don't know how
+	    		   
 	    		   if(y.indexOf(y.charAt(i))== "+")//if the string hit (+) operator, do the addition
 	    		     {
-	    		      result = y.charAt(i-1) + y.charAt(i+1);
+	    		      result = leftOp + rightOp;
 	    		     }
 	    		   else if(y.indexOf(y.charAt(i))== "-")//if the string hit (-) operator, do the addition
 	    		     {
-	    			  result = y.charAt(i-1) - y.charAt(i+1);
+	    			  result = leftOp - rightOp;
 	    		     }
 	    		   else if(y.indexOf(y.charAt(i))== "*")//if the string hit (*) operator, do the addition
 	    		     {
-	    			  result = y.charAt(i-1) * y.charAt(i+1);
+	    			  result = leftOp * rightOp;
 	    		     }
 	    		   else if(y.indexOf(y.charAt(i))== "/")//if the string hit (/) operator, do the addition
 	    		     {
-	    			  result = y.charAt(i-1) / y.charAt(i+1);
+	    			  result = leftOp / rightOp;
 	    		     }
 	    	     }
               }
-	     System.out.println(current);
          return result;     
 	    }       
         
