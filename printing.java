7f2b9445df8a57e9603505f4dd0f5640032d1646
Dalal Alshehri <Dalal Alshehri>
2016-02-17 10:59:10 -0600

    I tryied many times, this is it

--- printing.java
+++ printing.java
@@ -7,7 +7,7 @@ public class printing {
 	    //printVowels(x);
         //printConsonants(x);
         //printDigits(x);
-	    processExpression(y);
+	    System.out.println(processExpression(y));
     }
 
 	   
@@ -52,42 +52,55 @@ public class printing {
 
 //this method should take a String in the format: "integer [[+-*/] integer]*]"
 //then the method should process this expression and return the result as an integer
-static int processExpression(String y)
-        {
-    	   String current = "";//it helps to hold the current value
-    	   int result;//the expression total will held in this variable
-    	   int leftOp = 0; //this variable used to save the numbers on the left side of operator(=,-,*,/)
-    	   int rightOp = 0;//this variable used to save the numbers on the right side of operator(=,-,*,/)
-	       for(int i=0; i< y.length();i++)//for looping until the end of string 
-              {
-	    	   current = current + y.charAt(i);
-	    	   
-	    	   if(y.indexOf(i)!= -1)//to check that the string still has value
-	    	     {
-	    		   //here I need to hold the numbers on specific place, but I don't know how
-	    		   
-	    		   if(y.indexOf(y.charAt(i))== "+")//if the string hit (+) operator, do the addition
-	    		     {
-	    		      result = leftOp + rightOp;
-	    		     }
-	    		   else if(y.indexOf(y.charAt(i))== "-")//if the string hit (-) operator, do the addition
-	    		     {
-	    			  result = leftOp - rightOp;
-	    		     }
-	    		   else if(y.indexOf(y.charAt(i))== "*")//if the string hit (*) operator, do the addition
-	    		     {
-	    			  result = leftOp * rightOp;
-	    		     }
-	    		   else if(y.indexOf(y.charAt(i))== "/")//if the string hit (/) operator, do the addition
-	    		     {
-	    			  result = leftOp / rightOp;
-	    		     }
-	    	     }
-              }
-         return result;     
-	    }       
-        
- }
+  		static int processExpression(String y) 
+  		{ 
+  			
+  			String operator = "+-*/";//for saving the operators
+  			String numbers ="0123456789";//the possible numbers that might be included in the string
+  			char currentOp = '?';//current operators
+  			String rightNum ="" ;//the numbers that will be on the right side of operator
+  			String leftNum = "";//the numbers that will be on the left side of operator
+  			int result = 0;//for saving the result
+  			
+  			for(int i =0; i< y.length();i++)//for looping until the end of string )
+  			{
+  				if (numbers.indexOf(y.charAt(i))!= -1)//if the string is number, then add it to the current number
+  						leftNum = leftNum+ y.charAt(i);
+  				
+  				else if (operator.indexOf(y.charAt(i))!= -1)//if the operator still inside the string, then add it to current operator
+  					   {
+  					    //currentOp = y.charAt(i);
+  					    if(rightNum.length()==0)//to make sure that there isn't any number in the right side of operator
+  					      {
+  					    	rightNum = leftNum;//if we find nothing in the rightNum, then make it equal to the left number 
+  					    	leftNum ="";
+  					      }
+  					    else//if there is a number in the right side of operator, we can do the expression 
+  					      {
+  					    	  if(currentOp == '+')
+  					    	   {
+  					    	    result = leftNum + rightNum;// make the addition but I don't how to convert it to integer
+  					    	   }
+  					    	  if(currentOp == '-')
+  					    	   {
+  					    	    result = leftNum - rightNum;// make the addition but I don't how to convert it to integer
+  					    	   }
+  					    	  if(currentOp == '*')
+  					    	   {
+  					    	    result = leftNum * rightNum;// make the addition but I don't how to convert it to integer
+  					    	   }
+  					    	  if(currentOp == '/')
+  					    	   {
+  					    	    result = leftNum / rightNum;// make the addition but I don't how to convert it to integer
+  					    	   }
+  					       }
+  					   }
+  				
+  			}//end of for loop
+  			return result;
+  		}//end of method
+  		
+ }//end of class
     
     
 
